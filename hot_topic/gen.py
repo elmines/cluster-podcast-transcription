eabb6620084135c26d0af45609cbf3256c39156b
Ethan L. Mines <elmines@users.noreply.github.com>
2026-08-26 13:04:54 -0400

    Use topic gpt's prompt format for generating topics

--- hot_topic/gen.py
+++ hot_topic/gen.py
@@ -10,6 +10,12 @@ from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import xgrammar as xgr
 
+from .utils import partial_format, tokenized_with_trunc
+from .constants import GEN_USER_PROMPT, DEFAULT_TOPICS
+
+def format_topic(topic_name, topic_desc):
+    return f"{topic_name} : {topic_desc}"
+
 def main(raw_args=None):
     parser = argparse.ArgumentParser(description="Generate candidate topics")
     parser.add_argument("-i", type=os.path.abspath, default="out/resegmented")
@@ -24,22 +30,13 @@ def main(raw_args=None):
     model = AutoModelForCausalLM.from_pretrained(model_name).cuda()
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    system_prompt = r"""
-Answer with one topic and quotation from the transcript per line.
-The topic should be 1 to 30 characters.
-The substring should be 1 to 256 characters.
-Separate the two with a colon.
-Do not add quote marks to your substring
-"""
-
-    standard_messages = [
-        {"role": "system", "content": system_prompt},
-    ]
+    model_context_window = getattr(model.config, "max_position_embeddings", 96000)
+    max_new_tokens = 2048
+    max_input = model_context_window - max_new_tokens
 
 
     file_names = []
     texts = []
-
     # Sample one episode from each show
     show_dirs = [d_path for d_path in glob.glob(os.path.join(data_dir, "*")) if os.path.isdir(d_path)]
     for show_dir in tqdm(show_dirs, desc="Processing shows"):
@@ -47,76 +44,63 @@ Do not add quote marks to your substring
             with open(csv_path, 'r') as r:
                 texts.append("".join(row['text'] for row in csv.DictReader(r)))
             file_names.append(csv_path)
-
             # Debug statement
             break
 
 
-    output_pattern = re.compile('([a-z ]+) : (.+)')
-
+    output_pattern = re.compile(r'\[1\] ([a-z ]+) : ([a-z ]+) : (.+)')
+    # output_pattern = re.compile('([a-z ]+) : (.+)')
     tokenizer_info = xgr.TokenizerInfo.from_huggingface(tokenizer)
 
     grammar = xgr.GrammarCompiler(tokenizer_info).compile_grammar(r"""
 root ::=  () | entry ("\n" entry){0,2}
-entry ::= [a-z][a-z ]{0,29} " : " [^\r\n]{1,512}
-topic ::= [a-z][a-z ]{0,29}
+entry ::= "[1] " [a-z][a-z ]{0,29} " : " [a-z][a-z ]{0,255} " : " [^\r\n]{1,512}
     """)
+    system_prompt = r"""
+Answer with one topic per line.
+Use the following format:
+[1] topic : topic desc : episode quote
 
-
-    out_rows = [
-
-    ]
-
-    orig_topics = ["politics", "business", "sports", "entertainment"]
-    cur_topics = sorted(orig_topics)
-    set_topics = set(cur_topics)
-    text_iter = tqdm(texts, desc="Processing texts")
-    for file_path, text in zip(file_names, text_iter):
-        user_prefix = f"""
-You are tasked with performing topic modeling over podcast transcripts.
-Here are the topics you've discovered so far:
-{','.join(cur_topics)}
-
-Given a podcast transcript, generate a list of no more than 3 topics.
-Each topic should be on a separate line and be followed by a colon
-and a substring of the transcript as your justification for the topic.
-
-Consider the sample podcast transcript:
-\"I really want to take a Mediterranean cruise. There's so much history on the coasts of Greece and Turkey. And we can enjoy some good ouzo.\"
-
-Your output could be as follows:
-travel : want to take a Mediterranean cruise
-history : There's so much history
-food and drink : enjoy some good ouzo
-
-Now here's the actual transcript:
+The topic should be 1 to 30 characters.
+The topic description should be 1 to 256 characters.
+The episode quote should be 1 to 512 characters.
+Do not add quote marks to your episode quote.
 """
 
+    orig_topics = DEFAULT_TOPICS
+    set_topics = {t for (t,*_) in orig_topics}
+    topic_str = "\n".join( format_topic(name, desc) for name, desc in orig_topics)
 
-        tokenized_prompt = tokenizer.apply_chat_template(standard_messages + [
-            {"role": "user", "content": user_prefix + "\n" + text}
-        ],
-        tokenize=True, add_generation_prompt=True, return_tensors='pt').to(model.device)
+    out_rows = []
+    text_iter = tqdm(texts, desc="Processing texts")
+    i = 0
+    for file_path, text in zip(file_names, text_iter):
+        user_prefix = partial_format(GEN_USER_PROMPT, Topics=topic_str)
+        tokenized_prompt = tokenized_with_trunc(tokenizer,
+                                            [{"role": "system", "content": system_prompt}],
+                                            user_prefix,
+                                            text,
+                                            max_input).to(model.device)
         output = model.generate(**tokenized_prompt,
                                 logits_processor=[xgr.contrib.hf.LogitsProcessor(grammar)],
-                                max_new_tokens=2048
-        )
+                                max_new_tokens=max_new_tokens)
         input_length = tokenized_prompt['input_ids'].shape[-1]
         decoded = tokenizer.decode(output[0][input_length:], skip_special_tokens=True)
 
         # The quote they give must be from the text itself--avoid hallucinations
         matches = output_pattern.findall(decoded)
         # We ignore quotes that were hallucinated
-        valid_matches = [ (topic, quote) for (topic, quote) in matches if quote in text]
-        set_topics = set_topics | {t for t, _ in valid_matches}
-        cur_topics = sorted(set_topics)
+        valid_matches = [ (topic, desc, quote) for (topic, desc, quote) in matches if quote in text]
+        out_rows.extend((file_path, *match_res) for match_res in valid_matches)
+        if (new_topics := [(t, desc) for (t, desc, *_) in valid_matches if t not in set_topics]):
+            set_topics.update(t for t, _ in new_topics)
+            topic_str += "\n" + "\n".join(format_topic(name, desc) for name, desc in new_topics)
+        
+        if (i := i + 1) >= 5:
+            break
 
-        for topic, quote in valid_matches:
-            out_rows.append(
-                (file_path, topic, quote)
-            )
 
-    fields = ["episode_file", "topic", "episode_quote"]
+    fields = ["episode_file", "topic", "desc", "episode_quote"]
     with open(out_path, 'w') as w:
         writer = csv.writer(w)
         writer.writerow(fields)
---
+++ hot_topic/utils.py
@@ -0,0 +1,37 @@
+from typing import List, Any
+import sys
+
+from transformers import PreTrainedTokenizerFast
+
+############# From Chat GPT ########################
+class PartialFormatDict(dict):
+    def __missing__(self, key):
+        return "{" + key + "}"
+
+def partial_format(s, **kwargs):
+    return s.format_map(PartialFormatDict(kwargs))
+###################################################
+
+def tokenized_with_trunc(tokenizer: PreTrainedTokenizerFast,
+                         messages: List[Any],
+                         prompt_template: str,
+                         document: str,
+                         max_len: int):
+
+    trunc_document = document
+    while True:
+        prompt = tokenizer.apply_chat_template(
+            messages + [{"role": "user", "content": prompt_template.format(Document=trunc_document)}],
+            tokenize=True,
+            add_generation_prompt=True,
+            return_tensors='pt'
+        )
+        if (excess := prompt['input_ids'].shape[-1] - max_len) > 0:
+            trunc_document = tokenizer.decode(
+                tokenizer.convert_tokens_to_ids(tokenizer.tokenize(document))[:-excess]
+            )
+            if not trunc_document:
+                print("Prompt left no room for document", file=sys.stderr)
+        else:
+            break
+    return prompt
