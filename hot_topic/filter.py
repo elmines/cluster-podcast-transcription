1ee2bed73560f1052901efc521b8fd7a2aa597e1
Ethan L. Mines <elmines@users.noreply.github.com>
2026-08-27 10:56:16 -0400

    Update topic filtering

--- hot_topic/filter.py
+++ hot_topic/filter.py
@@ -1,15 +1,34 @@
 import argparse
 import os
 from itertools import combinations, batched
+import json
+import pdb
 
 from tqdm import tqdm
 import pandas as pd
+
+def filter_by_freq(topics_df: pd.DataFrame, quotes_df: pd.DataFrame, min_freq: int) -> pd.DataFrame:
+    topic_counts = quotes_df['topic'].value_counts()
+    mask = topics_df['topic'].apply(lambda t: topic_counts[t] >= min_freq)
+    return topics_df[mask]
 
 def main(raw_args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("-i-quotes", default="out/gen/scored_topic_quotes.csv", type=os.path.abspath)
     parser.add_argument("-i", default="out/gen/topics.csv", type=os.path.abspath)
+
     parser.add_argument("-o", default="out/gen/filtered_topics.csv", type=os.path.abspath)
+    parser.add_argument("-o-quote", default="out/gen/filtered_quotes.csv", type=os.path.abspath)
+    parser.add_argument("-o-merges", default="out/gen/topic_merges.json", type=os.path.abspath)
+
+    parser.add_argument("--min-freq",
+                        type=int,
+                        default=2,
+                        help="Minimum topic occurrence frequency PRIOR to merging. Set to 0 to ignore")
+    parser.add_argument("--min-freq-postmerge",
+                        type=int,
+                        default=0,
+                        help="Minimum topic occurrence frequence AFTER merging. Set to 0 to ignore")
 
     parser.add_argument("--quote-thresh",
                         type=float,
@@ -33,13 +52,20 @@ def main(raw_args=None):
     args = parser.parse_args(raw_args)
     quotes_path = args.i_quotes
     topics_path = args.i
-    out_path = args.o
-    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    out_topics_path = args.o
+    out_quotes_path = args.o_quote
+    out_merges_path = args.o_merges
     model_name = args.model
     quote_thresh = args.quote_thresh
     redund_thresh = args.redund_thresh
     embed_batch_size = args.embed_batch_size
     sim_batch_size = args.sim_batch_size
+    min_freq = args.min_freq
+    min_freq_postmerge = args.min_freq_postmerge
+
+
+    for op in [out_topics_path, out_quotes_path, out_merges_path]:
+        os.makedirs(os.path.dirname(op), exist_ok=True)
 
     topics_df = pd.read_csv(topics_path)
     old_len = len(topics_df)
@@ -63,6 +89,12 @@ def main(raw_args=None):
     topics_df = topics_df[topics_df['topic'].apply(attributed_topics.__contains__)]
     if (filtered_out := old_len - len(topics_df)):
         print(f"Filtered out {filtered_out} topics which have no attributable quote")
+
+    if (min_freq > 0):
+        old_len = len(topics_df)
+        topics_df = filter_by_freq(topics_df, quotes_df, min_freq)
+        if (filtered_out := old_len - len(topics_df)):
+            print(f"Filtered out {filtered_out} topics with frequency below {min_freq}")
 
     # For now doing a crude all-pairs comparison
     if (redund_thresh < 1):
@@ -107,22 +139,43 @@ def main(raw_args=None):
             print(f"Merge: {score:.3f},{topics_by_inds[ind_a]},{topics_by_inds[ind_b]}")
 
 
-        reduced_topics = set()
+        topic_remapping = dict()
         for subset in ds.subsets():
             str_subset = [topics_by_inds[i] for i in subset]
             # Use the shortest topic name as the rep
             simple_name = min(str_subset, key=len)
-            reduced_topics.add(simple_name)
+            topic_remapping.update({s:simple_name for s in str_subset})
             if len(str_subset) > 1:
                 print(f"Final set: {str_subset} --> {simple_name}")
 
+        with open(out_merges_path, 'w') as w:
+            json.dump(topic_remapping, w, indent=2)
+        print(f"Wrote {out_merges_path}")
+
+        reduced_topics = set(topic_remapping.values())
         old_len = len(topics_df)
         topics_df = topics_df[topics_df['topic'].apply(reduced_topics.__contains__)]
         if (filtered_out := old_len - len(topics_df)):
             print(f"Filtered out {filtered_out} topics based on embedding similarity")
 
-    topics_df.to_csv(out_path)
+        quotes_df['topic'] = quotes_df['topic'].apply(topic_remapping.__getitem__)
 
+    if (min_freq_postmerge > 0):
+        old_len = len(topics_df)
+        topics_df = filter_by_freq(topics_df, quotes_df, min_freq_postmerge)
+        if (filtered_out := old_len - len(topics_df)):
+            print(f"Filtered out {filtered_out} topics with frequency below {min_freq_postmerge}")
+
+    topics_df.to_csv(out_topics_path)
+    print(f"Wrote {out_topics_path}")
+
+    rem_topics = set(topics_df['topic'])
+    old_len = len(quotes_df)
+    quotes_df = quotes_df[quotes_df['topic'].apply(rem_topics.__contains__)]
+    if (filtered_out := old_len - len(quotes_df)):
+        print(f"Filtered out {filtered_out} quotes that no longer have a topic")
+    quotes_df.to_csv(out_quotes_path)
+    print(f"Wrote {out_quotes_path}")
 
 if __name__ == "__main__":
     main()
